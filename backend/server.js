3d3bf7d73939d8143c03f728a067a4ea52e1e57a
unknown <jekarr2020@gmail.com>
2022-02-09 15:31:28 +0530

    node env

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,19 @@ app.use(errorHandler)
 //const __dirname = path.resolve()
 //app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use("/uploads", express.static("uploads"))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
